8c972f605c1672545a7cfcb9d5b7db49bd6d58a2
Tu Nguyen <vantu1998@gmail.com>
2022-04-14 00:31:09 +0700

    feat(web): add angular material

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { API_BASE_URL } from '@nx-with-chau-tran/web/shared-data-access-api-sdk';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [{ provide: API_BASE_URL, useValue: environment.apiBaseUrl }],
   bootstrap: [AppComponent],
 })
