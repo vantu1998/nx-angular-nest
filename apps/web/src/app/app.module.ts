12ebdb715f1ef189d241e4a75a051814bddf73a5
Tu Nguyen <vantu1998@gmail.com>
2022-04-14 00:09:14 +0700

    feat(web): add nswag

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { API_BASE_URL } from '@nx-with-chau-tran/web/shared-data-access-api-sdk';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [{ provide: API_BASE_URL, useValue: environment.apiBaseUrl }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
