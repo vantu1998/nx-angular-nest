963befa39735cdfecc44325a541ece6ae48eefe3
Nguyen Van Tu <vantu1998@gmail.com>
2022-06-30 17:11:19 +0700

    feat(web): create ui for navbar-left

--- libs/web/shell/feature/src/lib/layout.component.ts
+++ libs/web/shell/feature/src/lib/layout.component.ts
@@ -3,10 +3,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import {NavbarLeftComponentModule} from '@nx-with-chau-tran/web/shell/ui/navbar-left';
 
 @Component({
   selector: 'ct-layout',
-  template: ``,
+  template: `   
+    <ct-navbar-left></ct-navbar-left>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent {
@@ -15,7 +18,7 @@ export class LayoutComponent {
 @NgModule({
   declarations: [LayoutComponent],
 
-  imports: [MatToolbarModule, RouterModule, MatMenuModule, MatIconModule],
+  imports: [MatToolbarModule, RouterModule, MatMenuModule, MatIconModule, NavbarLeftComponentModule],
   exports: [LayoutComponent],
 })
 export class LayoutComponentModule {}
--- libs/web/shell/ui/navbar-left/src/lib/navbar-left.component.ts
+++ libs/web/shell/ui/navbar-left/src/lib/navbar-left.component.ts
@@ -1,15 +1,58 @@
-import {ChangeDetectionStrategy, Component, NgModule} from '@angular/core';
+import {ChangeDetectionStrategy, Component, NgModule, OnInit} from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: `ct-navbar-left`,
-  template:`
+  template: `
+    <div class="w-[64px] bg-[#0747a6] h-screen flex items-center py-8 flex-col color-white">
+      <div class="py-4">
+        <img src="assets/logo.svg" alt="Logo" class="w-10 h-10">
+      </div>
+      <div *ngFor="let item of navItems" [matTooltip]="item.tooltip" matTooltipPosition="right" class="my-1">
+        <mat-icon class="text-white">{{item.icon}}</mat-icon>
+      </div>
+      <div class="flex-auto"></div>
+      <div matTooltip="Admin user" matTooltipPosition="right" class="border-2 border-solid border-transparent hover:border-blue-500 rounded-full w-10 h-10">
+        <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+             alt="avatar" class="rounded-full object-cover object-center">
+      </div>
+      <div class="text-white w-8 h-8 my-2 border-4 border-solid border-transparent hover:border-blue-500 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-full h-full" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+        </svg>
+      </div>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarLeftComponent{}
+export class NavbarLeftComponent implements OnInit {
+  navItems!: NavItem[];
+
+  ngOnInit() {
+    this.navItems = [
+      new NavItem('search', 'Search issues'),
+      new NavItem('add', 'Create issue')
+    ]
+  }
+}
+
+class NavItem {
+  constructor(public icon: string, public tooltip: string, public handler?: Handler) {
+  }
+}
+
+type Handler = () => void;
 
 @NgModule({
+  imports: [
+    CommonModule,
+    MatTooltipModule,
+    MatIconModule
+  ],
   declarations: [NavbarLeftComponent],
   exports: [NavbarLeftComponent]
 })
-export class NavbarLeftComponentModule{}
+export class NavbarLeftComponentModule {
+}
